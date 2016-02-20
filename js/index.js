5d58632e3083aeb425683d3c5bc3b8c1157d4718
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2016-02-20 11:43:08 +0300

    Replace hand-made with real cyclejs packages

--- js/index.js
+++ js/index.js
@@ -1,62 +1,26 @@
 import Cycle from '@cycle/core';
+import CycleDOM, {
+  h,
+  h1,
+  span,
+  makeDOMDriver
+} from '@cycle/dom';
+
 import Rx from 'rx';
 import styles from './index.css';
 
-function h(tagName, children) {
-  return { 
-    tagName,
-    children
-  };
-}
-
 function main(sources) {
-  const over$ = sources.DOM.selectEvents('span', 'mouseover');
+  const over$ = sources.DOM.select('span').events('mouseover');
   const sinks = {
     DOM: over$
       .startWith(null)
       .flatMapLatest(() => Rx.Observable.timer(0, 1000).map(i =>
-        h('h1', [h('span', [`elapsed: ${i}`])])
+        h1([span([`elapsed: ${i}`])])
       )),
     Log: Rx.Observable.timer(0, 2000).map(i => i * 2)
   };
 
   return sinks;
-}
-
-function makeDOMDriver(mountSelector) {
-  return function DOMDriver(obj$) {
-    function createElement(element) {
-      const { tagName, children } = element;
-      const el = document.createElement(tagName);
-
-      children
-        .filter(c => typeof c === 'object')
-        .map(createElement)
-        .forEach(c => el.appendChild(c));
-
-      children
-        .filter(c => typeof c === 'string')
-        .forEach(s => el.innerHTML += s);
-
-      return el;
-    }
-
-    obj$.subscribe(obj => {
-      const container = document.querySelector(mountSelector);
-      container.innerHTML = '';
-      const el = createElement(obj);
-      container.appendChild(el);
-    });
-
-    const DOMSource = {
-      selectEvents: (tagName, eventType) =>
-        Rx.Observable
-          .fromEvent(document, eventType)
-          .filter(e => e.target.tagName === tagName.toUpperCase())
-    };
-
-    return DOMSource;
-  }
 }
 
 const consoleLogDriver = text$ =>
